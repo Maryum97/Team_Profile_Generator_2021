ba0daeaf4f6e4091bf1a45459026c8fcc352574c
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-02-23 15:48:25 +0000

    Define Modules

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Maryum here.");
+// Define all the packages and modules required for this application
+const inquirer = require('inquirer');
+const jest = require('jest');
+const fs = require('fs');
+const Employee = require('./tests/Employee.test');
+const Engineer = require('./tests/Engineer.test');
+const Intern = require('./tests/Intern.test');
+const Manager = require('./tests/Manager.test');
+
+// Log out any necessities to check for errors in terminal
+console.log("Hi, I'm starting up my assignment.");
+console.log(typeof Employee);
+console.log(typeof Engineer);
+console.log(typeof Intern);
+console.log(typeof Manager);
