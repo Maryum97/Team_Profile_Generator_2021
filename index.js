efd578cb6a66a36e513a3bd0257df7bcb098c185
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-02-24 00:36:15 +0000

    Add Prompts for Node.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Manager = require('./library/Manager');
 
 // Log out any necessities to check for errors in terminal
 console.log("Hi, I'm starting up my assignment.");
-console.log(typeof(inquirer), typeof(jest), typeof(fs));
+console.log(typeof (inquirer), typeof (jest), typeof (fs));
 console.log(typeof Employee, typeof Engineer, typeof Intern, typeof Manager);
 
 // Define array for all employees working in the team
@@ -18,16 +18,41 @@ const employees = [];
 // DECLARE FUNCTIONS BELOW:
 
 // Declare funtion to initialise the application and add employees to the app page
-function initialiseApp () {
+function initialiseApp() {
     // Call function to start the HTML page
     startHTML();
     // Call funtion to add employees to the page
-    addEmployees();
+    addEmployee();
 }
 
 // Declare function to add employees to the array
-function addEmployees() {
+function addEmployee() {
+    inquirer.prompt([
+        {
+            // Propmpt for employee name
+            message: "Enter the name of the team member:",
+            name: "name"
+        },
+        {
+            // Prompt for employee role
+            type: "list",
+            message: "Select the team member's role:",
+            name: "role",
+            choices: ["Engineer", "Intern", "Manager"]
+        },
+        {
+            // Prompt for employee ID
+            message: "Enter the ID of the team member:",
+            name: "ID"
+        },
+        {
+            // Prompt for employee e-mail
+            message: "Enter the e-mail address of the team member:",
+            name: "e-mail"
+        }
+    ]).then(function(name, role, id, email) {
 
+    })
 }
 
 // Declare function to start rendering of the HTML page
